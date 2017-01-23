98db4c166f132e0536d18821eecbbff175dbcffc
DESKTOP-3PMU691\Nqobani Zulu <nqobanizulu15@gmail.com>
2017-01-23 16:55:55 +0200

    T

--- Slingshot/Slingshot.LogicLayer/DbConnection.cs
+++ Slingshot/Slingshot.LogicLayer/DbConnection.cs
@@ -131,10 +131,6 @@ namespace Slingshot.Data
             return recipient;
         }
 
-
-
-
-
         public Campaign createCampaign(string creatorId, string name,Boolean prefared, string thumbnail,  string status = "public")
         {
             Campaign newCampaign = new Campaign();
