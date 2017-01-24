cf8120c0cb786da3e1b986d7295ecbfea95d5eb3
DESKTOP-3PMU691\Nqobani Zulu <nqobanizulu15@gmail.com>
2017-01-24 09:10:15 +0200

    Changing api key

--- Slingshot/Slingshot.Data/Services/UserService.cs
+++ Slingshot/Slingshot.Data/Services/UserService.cs
@@ -188,7 +188,7 @@ namespace Slingshot.Data.Services
 
         public async Task SendEmail(string fromEmail, string toEmail, string subj, long vCardId, string HTML, Data.Models.Attachment[] emailAttechments)
         {
-            string apiKey = "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";//Environment.GetEnvironmentVariable("sendgrid_api_key", EnvironmentVariableTarget.User);
+            string apiKey = "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";//Environment.GetEnvironmentVariable("sendgrid_api_key", EnvironmentVariableTarget.User);
             dynamic sg = new SendGridAPIClient(apiKey);
 
             SendGrid.Helpers.Mail.Email from = new SendGrid.Helpers.Mail.Email(fromEmail);
